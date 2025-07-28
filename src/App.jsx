f1ad639295ba1ba27590e63cd035e460b560310c
YashviKalathiya17 <yashvikalathiya402@gmail.com>
2025-07-28 22:36:48 +0530

    "28-07-2025"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Header from "./components/header"
-import Footer from "./components/footer"
 import './App.css'
+import Header from "./components/header"
 import Hero from "./components/Hero"
+import Footer from "./components/footer"
+
 
 function App() {
   return (
     <>
-      <Header/>
-      <h1>hello</h1>
-      <Hero/>
-      <Footer/>
+      <Header />
+      {/* <h1>hello</h1> */}
+      <Hero />
+      <Footer />
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,21 @@
-const Hero=()=>{
-    return(
+const Hero = () => {
+    return (
         <>
+
+            <section>
+                <div className="img1 ">
+                    <div>
+                        <p className="date1">10/04/14 - 10/05/14</p>
+                    </div>
+                    <div className="text-center mt-3 fs-1 text1">
+                        MID SEASON SALE
+                    </div>
+                    <div className="text2 text-center container">UP TO <b className="text3">50</b>%OFF</div>
+                    <div className="text-center text-white fs-5 w-50 mx-auto ">
+                        Mauris commodo nisl eu odio luctus, sed iaculis orci tincidunt. <br /> Suspendisse vulputate luctus.
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
